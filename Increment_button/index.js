7003756076c3a79598d4cfad6a428f115ec25ed8
Erbasher <106031246+Erbasher@users.noreply.github.com>
2025-01-19 15:42:00 +0200

    first commit

---
+++ Increment_button/index.js
@@ -0,0 +1,9 @@
+// initialize the count as 0
+// listen for clicks on the increment button
+// increment the count variable when the button is clicked
+// change the count-el in the HTML
+
+
+function increment () {
+    console.log("The button was clicked");
+}
